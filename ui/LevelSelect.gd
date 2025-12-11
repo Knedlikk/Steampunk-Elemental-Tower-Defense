af5362a389261c131b451a906da2732e1ed535c2
Ondra00 <ondramikyska@gmail.com>
2025-12-11 23:32:38 +0100

    Poslední úpravy
    odstranění nepotřebných složek, nastavení hlavní scény, přidání soundtracku k levelům, přidání exit buttonu do každého levelu

--- ui/LevelSelect.gd
+++ ui/LevelSelect.gd
@@ -1,15 +1,15 @@
 extends Control
 
 func _on_level_1_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Levels/Level1.tscn")
+	get_tree().change_scene_to_file("res://Urovne/level_1.tscn")
 
 
 func _on_level_2_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Levels/Level2.tscn")
+	get_tree().change_scene_to_file("res://Urovne/level_2.tscn")
 
 
 func _on_level_3_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Levels/Level3.tscn")
+	get_tree().change_scene_to_file("res://Urovne/level_3.tscn")
 
 
 func _on_back_button_pressed() -> void:
